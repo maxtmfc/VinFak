6d9c63e621cccccc4531acb38bd89e867ab1de54
Anya <bugelskaya@yandex.ru>
2023-04-20 18:51:47 +0300

    initial routes

---
+++ client/src/components/Pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainPage(): JSX.Element {
+  return (
+    <div>
+      MainPage
+    </div>
+  )
+}
---
+++ client/src/components/UI/OneUser.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OneUser(): JSX.Element {
+  return (
+    <div>
+      OneUser
+    </div>
+  )
+}
